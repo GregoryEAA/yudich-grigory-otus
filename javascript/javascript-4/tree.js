f13d926aa7a9166eadb8bfd64eb8e5d6423f73b0
Юдич Григорий Эмилевич <g.yud@incoma.ru>
2023-06-19 14:03:15 +0300

    javascript-4

---
+++ javascript/javascript-4/tree.js
@@ -0,0 +1,32 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function tree(dirPath) {
+    let result = {
+        files: [],
+        dirs: [],
+    };
+
+    async function walk(currentPath) {
+        const entries = await fs.readdir(currentPath, { withFileTypes: true });
+
+        for (const entry of entries) {
+            const entryPath = path.join(currentPath, entry.name);
+
+            if (entry.isDirectory()) {
+                result.dirs.push(entryPath);
+                await walk(entryPath);
+            } else if (entry.isFile()) {
+                result.files.push(entryPath);
+            }
+        }
+    }
+
+    await walk(dirPath);
+    return result;
+}
+
+const folderPath = process.argv[2] || '.';
+tree(folderPath)
+    .then((result) => console.log(JSON.stringify(result, null, 2)))
+    .catch((error) => console.error('Error:', error));
